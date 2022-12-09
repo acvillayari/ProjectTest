87d2bf9784d5c4f297f2ec191fd2953cd2dca98c
acvillayari <acvillayari@gmail.com>
2022-12-08 22:17:07 -0500

    add app engine

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,13 +11,13 @@ var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get now");
-    return "GET - Net 6 Running ... 06122022-v1.0.0 CI/CD in TRAVIS v1.0.0 AFORO255";
+    return "GET - Net 6 Running ... 06122022-v1.0.0 CI/CD in TRAVIS  Alexander Villalobos v1.0.0 AFORO255";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ... 06122022-v1.0.0 CI/CD in TRAVIS v1.0.0 AFORO255";
+    return "POST - Net 6 Running ... 06122022-v1.0.0 CI/CD in TRAVIS Alexander Villalobos v1.0.0 AFORO255";
 });
 
 app.Run($"http://0.0.0.0:{port}");
